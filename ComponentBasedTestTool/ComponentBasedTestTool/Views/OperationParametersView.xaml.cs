52eb32f3da50e06aafe2af18692755d5cbd575ba
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2016-01-02 11:55:39 +0100

    Moved parameters to user control

---
+++ ComponentBasedTestTool/ComponentBasedTestTool/Views/OperationParametersView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComponentBasedTestTool.Views
+{
+  /// <summary>
+  /// Interaction logic for OperationParametersView.xaml
+  /// </summary>
+  public partial class OperationParametersView : UserControl
+  {
+    public OperationParametersView()
+    {
+      InitializeComponent();
+    }
+  }
+}
