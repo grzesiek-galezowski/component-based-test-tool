afbcd94bb2bc1c9bf076decce84fd06106793e29
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-07-30 17:59:41 +0200

    Update GivenTwoComponentInstancesAreLoaded.cs

--- ComponentBasedTestTool/ComponentSpecification/GivenTwoComponentInstancesAreLoaded.cs
+++ ComponentBasedTestTool/ComponentSpecification/GivenTwoComponentInstancesAreLoaded.cs
@@ -1,12 +1,11 @@
 using ComponentSpecification.AutomationLayer;
-using Xbehave;
+using Xunit;
 using static ComponentSpecification.ComponentAny;
 
 namespace ComponentSpecification;
 
 public class GivenTwoComponentInstancesAreLoaded
 {
-  private readonly ComponentBasedTestToolDriver _context = new();
   private readonly string _componentName1 = Any.ComponentName();
   private readonly string _componentName2 = Any.ComponentName();
   private readonly string _operationName11 = Any.OperationName();
@@ -20,73 +19,79 @@ public class GivenTwoComponentInstancesAreLoaded
   private readonly string _parameterValue3 = Any.ParameterValue();
   private readonly string _parameterValue4 = Any.ParameterValue();
 
-  [Background]
-  public void Bg()
-  {
-    "Given two components are loaded".x(() =>
-    {
-      _context.ComponentsSetup.Add(_componentName1)
-        .WithOperation(_operationName11)
-        .WithParameter(_parameterName1, _parameterValue1)
-        .WithParameter(_parameterName2, _parameterValue2);
-      _context.ComponentsSetup.Add(_componentName2)
-        .WithOperation(_operationName21)
-        .WithParameter(_parameterName3, _parameterValue3)
-        .WithParameter(_parameterName4, _parameterValue4);
-
-      _context.StartApplication();
-    });
-
-    "And I added one instance of each component"
-      .x(() =>
-      {
-        _context.ComponentsView.AddInstanceOf(_componentName1);
-        _context.ComponentsView.AddInstanceOf(_componentName2);
-      });
-
-  }
-
-  [Scenario]
+  [Fact]
   public void ShouldDisplayOperationsOnScriptViewInOrderOfAddition()
   {
-    "When I add operation from the first component instance to script view"
-      .x(() => _context.AddToScriptView(_componentName1, _operationName11));
-    "And I add this operation again"
-      .x(() => _context.AddToScriptView(_componentName1, _operationName11));
-    "And I add an operation from the second component instance to script view"
-      .x(() => _context.AddToScriptView(_componentName2, _operationName21));
-    "Then the script view should display the three operations in order"
-      .x(() => _context.ScriptView.AssertShowsExactly(_operationName11, _operationName11, _operationName21));
+    var context = new ComponentBasedTestToolDriver();
+    context.ComponentsSetup.Add(_componentName1)
+      .WithOperation(_operationName11)
+      .WithParameter(_parameterName1, _parameterValue1)
+      .WithParameter(_parameterName2, _parameterValue2);
+    context.ComponentsSetup.Add(_componentName2)
+      .WithOperation(_operationName21)
+      .WithParameter(_parameterName3, _parameterValue3)
+      .WithParameter(_parameterName4, _parameterValue4);
+
+    context.StartApplication();
+
+    context.ComponentsView.AddInstanceOf(_componentName1);
+    context.ComponentsView.AddInstanceOf(_componentName2);
+
+    context.AddToScriptView(_componentName1, _operationName11);
+    context.AddToScriptView(_componentName1, _operationName11);
+    context.AddToScriptView(_componentName2, _operationName21);
+    context.ScriptView.AssertShowsExactly(_operationName11, _operationName11, _operationName21);
   }
 
-  [Scenario]
+  [Fact]
   public void ShouldDisplayOperationPropertiesOnScriptView()
   {
-    "When I add operation from the first component instance to script view"
-      .x(() => _context.AddToScriptView(_componentName1, _operationName11));
-    "And I select the operation on script view"
-      .x(() => _context.ScriptView.Select(_componentName1, _operationName11));
-    "Then the properties view should display properties of this operation"
-      .x(() => _context.PropertiesView.AssertShowsExactly(
-        Property(_parameterName1, _parameterValue1),
-        Property(_parameterName2, _parameterValue2)));
+    var driver = new ComponentBasedTestToolDriver();
+    driver.ComponentsSetup.Add(_componentName1)
+      .WithOperation(_operationName11)
+      .WithParameter(_parameterName1, _parameterValue1)
+      .WithParameter(_parameterName2, _parameterValue2);
+    driver.ComponentsSetup.Add(_componentName2)
+      .WithOperation(_operationName21)
+      .WithParameter(_parameterName3, _parameterValue3)
+      .WithParameter(_parameterName4, _parameterValue4);
+
+    driver.StartApplication();
+
+    driver.ComponentsView.AddInstanceOf(_componentName1);
+    driver.ComponentsView.AddInstanceOf(_componentName2);
+
+    driver.AddToScriptView(_componentName1, _operationName11);
+    driver.ScriptView.Select(_componentName1, _operationName11);
+    driver.PropertiesView.AssertShowsExactly(
+      Property(_parameterName1, _parameterValue1),
+      Property(_parameterName2, _parameterValue2));
   }
 
-
-  [Scenario]
+  [Fact]
   public void ShouldKeepDisplayingPropertiesOfLastSelectedOperationEvenAfterAnotherComponentIsSelected()
   {
-    $"And I selected instance {_componentName1}"
-      .x(() => _context.InstancesView.Select(_componentName1));
-    $"And I selected its operation {_operationName11}"
-      .x(() => _context.OperationsView.Select(_operationName11));
-    $"And I added the operation {_operationName11} to script view"
-      .x(() => _context.AddToScriptView(_componentName1, _operationName11));
-    $"When I selected instance {_componentName2}"
-      .x(() => _context.InstancesView.Select(_componentName2));
-    $"Then the properties view should still show properties of {_operationName11}"
-      .x(() => _context.PropertiesView.AssertShowsExactly(
-        Property(_parameterName1, _parameterValue1), 
-        Property(_parameterName2, _parameterValue2)));
+    var context = new ComponentBasedTestToolDriver();
+    context.ComponentsSetup.Add(_componentName1)
+      .WithOperation(_operationName11)
+      .WithParameter(_parameterName1, _parameterValue1)
+      .WithParameter(_parameterName2, _parameterValue2);
+    context.ComponentsSetup.Add(_componentName2)
+      .WithOperation(_operationName21)
+      .WithParameter(_parameterName3, _parameterValue3)
+      .WithParameter(_parameterName4, _parameterValue4);
+
+    context.StartApplication();
+
+    context.ComponentsView.AddInstanceOf(_componentName1);
+    context.ComponentsView.AddInstanceOf(_componentName2);
+
+    context.InstancesView.Select(_componentName1);
+    context.OperationsView.Select(_operationName11);
+    context.AddToScriptView(_componentName1, _operationName11);
+    context.InstancesView.Select(_componentName2);
+    context.PropertiesView.AssertShowsExactly(
+      Property(_parameterName1, _parameterValue1),
+      Property(_parameterName2, _parameterValue2));
   }
 }
