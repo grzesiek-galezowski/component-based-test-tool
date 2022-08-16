6adb08bf8e338a368af11775849464f79a6f856c
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-08-16 08:52:12 +0200

    Update ListPipelinesOperation.cs

--- ComponentBasedTestTool/Components/AzurePipelines/ListPipelinesOperation.cs
+++ ComponentBasedTestTool/Components/AzurePipelines/ListPipelinesOperation.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using ExtensionPoints.ImplementedByComponents;
 using ExtensionPoints.ImplementedByContext;
+using Flurl.Http;
+using Playground;
 
 namespace Components.AzurePipelines;
 
@@ -10,7 +14,15 @@ public class ListPipelinesOperation : IComponentOperation
 {
   public async Task RunAsync(CancellationToken token)
   {
-    throw new NotImplementedException();
+    var organization = "grzesiekgalezowski";
+    var project = "grzesiekgalezowski";
+
+// LIST of pipelines request
+    var jsonAsync = await $"https://dev.azure.com/{organization}/{project}/_apis/pipelines?api-version=7.1-preview.1"
+      .GetJsonAsync<ListOfPipelines>();
+
+    var pipelineIds = jsonAsync.Value.Select(p => p.Id);
+    Console.WriteLine(string.Join(',', pipelineIds));
   }
 
   public void InitializeParameters(IOperationParametersListBuilder parameters)
